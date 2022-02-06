e04a2b6625d9d20aa84ae68ee67a341b546385fd
Firman Ismail <akayasha99@gmail.com>
2022-02-06 20:09:53 +0700

    tugas 19 js

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+var x = 'semangat...'
+console.log(x.repeat(5));
+
+var y =('* ')
+console.log(y.repeat(5));
+console.log(y.repeat(25));
+console.log(y.repeat(45));
+console.log(y.repeat(65));
+console.log(y.repeat(95));
+console.log(y.repeat(65));
+console.log(y.repeat(45));
+console.log(y.repeat(25));
+console.log(y.repeat(5));
